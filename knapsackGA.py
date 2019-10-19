8451f5c1de08327ebe5487d7bc2961b535526a80
AhmedEhabH <ahmed.ehab.h@gmail.com>
2019-10-19 13:35:15 +0200

    addd condition if self.population == self.pop_copy: break in end of while

--- knapsackGA.py
+++ knapsackGA.py
@@ -121,6 +121,7 @@ class Knapsack():
                 profits)
             index = profits.index(self.my_optimal)
             cromosome = self.pop_copy[index]
+            if self.population == self.pop_copy: break
             self.population = self.pop_copy.copy()
         print("Optimal after {0} iteration(s).".format(iteration))
         print("Optimal from knapsack GA", self.my_optimal)
